e4226f7e5c0dc4e76a53f36604e019a46c19a766
ohsuyuki <papu8osu@gmail.com>
2020-02-24 17:16:34 +0900

    initial commit

--- AtCoderB20200224/main.cpp
+++ AtCoderB20200224/main.cpp
@@ -8,8 +8,22 @@
 
 #include <iostream>
 
+int calcDigit(int N, int K, int d) {
+    if(N < K) {
+        return d+1;
+    } else {
+        return calcDigit(N/K, K, d+1);
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int N(0), K(0);
+
+    std::cin >> N >> K;
+
+    int digit = calcDigit(N, K, 0);
+
+    std::cout << digit;
+
     return 0;
 }
